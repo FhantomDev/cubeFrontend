197b640379fb709b35f6a29b2ec82b4d0f32a214
Irvin Cribillero Luna <127951672+FhantomDev@users.noreply.github.com>
2025-10-30 16:10:58 -0300

    Conectado con athena aws

--- src/pages/categoria/levelDefs.js
+++ src/pages/categoria/levelDefs.js
@@ -1,33 +1,33 @@
 export const levelDefs = {
   0: {
     dimensions: [
-      "main.product_categories_name",
+      "detalle_factura.categoria",
     ],
-    measures: ["main.line_items_sum_price", "main.line_items_sum_quantity"],
+    measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
-      { headerName: "Categoría de Producto", valueGetter: p => p.data ? p.data["main.product_categories_name"] : '', enableRowGroup: true },
-      { headerName: "Precio Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_price"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Cantidad Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_quantity"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Categoría de Producto", valueGetter: p => p.data ? p.data["detalle_factura.categoria"] : '', enableRowGroup: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
     ],
-    drillDownField: "main.product_categories_name",
+    drillDownField: "detalle_factura.categoria",
   },
   1: {
-    dimensions: ["main.products_name"],
-    measures: ["main.line_items_sum_price", "main.line_items_sum_quantity"],
+    dimensions: ["detalle_factura.nombre_producto"],
+    measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
-      { headerName: "Nombre de Producto", valueGetter: p => p.data ? p.data["main.products_name"] : '', enableRowGroup: true },
-      { headerName: "Precio Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_price"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Cantidad Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_quantity"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Nombre de Producto", valueGetter: p => p.data ? p.data["detalle_factura.nombre_producto"] : '', enableRowGroup: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
     ],
-    drillDownField: "main.products_name",
+    drillDownField: "detalle_factura.nombre_producto",
   },
   2: {
-    dimensions: ["main.users_state"],
-    measures: ["main.line_items_sum_price", "main.line_items_sum_quantity"],
+    dimensions: ["detalle_factura.id_cliente"],
+    measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
-      { headerName: "Estados", valueGetter: p => p.data ? p.data["main.users_state"] : '', enableRowGroup: true },
-      { headerName: "Precio Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_price"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Cantidad Total", valueGetter: p => p.data ? Number(p.data["main.line_items_sum_quantity"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "ID Cliente", valueGetter: p => p.data ? p.data["detalle_factura.id_cliente"] : '', enableRowGroup: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
     ],
   },
 };
