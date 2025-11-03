a871f7e0993a1649d3b82d446cbdbfb95c6dc1bf
Irvin Cribillero Luna <127951672+FhantomDev@users.noreply.github.com>
2025-11-03 15:20:37 -0300

    Añadido formato moneda para las columnas con ese valor

--- src/pages/categoria/levelDefs.js
+++ src/pages/categoria/levelDefs.js
@@ -1,3 +1,8 @@
+const currencyFormatter = new Intl.NumberFormat('es-CL', {
+  style: 'currency',
+  currency: 'CLP',
+});
+
 export const levelDefs = {
   0: {
     dimensions: [
@@ -6,8 +11,8 @@ export const levelDefs = {
     measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
       { headerName: "Categoría de Producto", valueGetter: p => p.data ? p.data["detalle_factura.categoria"] : '', enableRowGroup: true },
-      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
     ],
     drillDownField: "detalle_factura.categoria",
   },
@@ -16,8 +21,8 @@ export const levelDefs = {
     measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
       { headerName: "Nombre de Producto", valueGetter: p => p.data ? p.data["detalle_factura.nombre_producto"] : '', enableRowGroup: true },
-      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
     ],
     drillDownField: "detalle_factura.nombre_producto",
   },
@@ -26,8 +31,8 @@ export const levelDefs = {
     measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
     columnDefs: [
       { headerName: "ID Cliente", valueGetter: p => p.data ? p.data["detalle_factura.id_cliente"] : '', enableRowGroup: true },
-      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true },
-      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true },
+      { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+      { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
     ],
   },
 };
