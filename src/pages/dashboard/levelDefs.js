27f636e59acb1396eafcbc162dc4121a349b72df
Irvin Cribillero Luna <cribillerodev@gmail.com>
2025-11-05 17:19:45 -0300

    Arreglado error de NaN en calgunas columnas de la vista cliente

--- src/pages/dashboard/levelDefs.js
+++ src/pages/dashboard/levelDefs.js
@@ -49,11 +49,20 @@ export const levelDefs = {
   cliente: {
     0: {
       dimensions: ["detalle_factura.id_cliente"],
-      measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum"],
+      measures: ["detalle_factura.valor_neto_sum", "detalle_factura.costo_estandar_sum",
+        "detalle_factura.dcto_ajuste_sum",
+        "detalle_factura.dcto_promo_venta_sum",
+        "detalle_factura.factor_conversion_sum",
+        "detalle_factura.peso_neto_sum",
+      ],
       columnDefs: [
         { headerName: "ID Cliente", valueGetter: p => p.data ? p.data["detalle_factura.id_cliente"] : '', enableRowGroup: true },
         { headerName: "Valor Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
         { headerName: "Costo Estandar", valueGetter: p => p.data ? Number(p.data["detalle_factura.costo_estandar_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+        { headerName: "Descuento Ajuste", valueGetter: p => p.data ? Number(p.data["detalle_factura.dcto_ajuste_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+        { headerName: "Descuento Promo Venta", valueGetter: p => p.data ? Number(p.data["detalle_factura.dcto_promo_venta_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+        { headerName: "Factor Conversion", valueGetter: p => p.data ? Number(p.data["detalle_factura.factor_conversion_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value) },
+        { headerName: "Peso Neto", valueGetter: p => p.data ? Number(p.data["detalle_factura.peso_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => `${p.value.toFixed(2)} kg` },
       ],
       drillDownField: "detalle_factura.id_cliente",
     },
