f9ef29470c0a59a29803d451e2a5e55771bf0d6d
Irvin Cribillero Luna <cribillerodev@gmail.com>
2025-11-19 12:27:15 -0300

    Ajustes estilos

--- src/pages/dashboard/levels/numero_factura.js
+++ src/pages/dashboard/levels/numero_factura.js
@@ -31,7 +31,7 @@ export const numero_factura = {
       "detalle_factura.combinacion_sku_cliente"
     ],
     columnDefs: [
-      { headerName: "Factura", field: "detalle_factura.numero_factura_view", valueGetter: p => p.data ? p.data["detalle_factura.numero_factura_view"] : '', enableRowGroup: true, filter: 'agSetColumnFilter', minWidth: 200, pinned: 'left' },
+      { headerName: "Factura", field: "detalle_factura.numero_factura_view", valueGetter: p => p.data ? p.data["detalle_factura.numero_factura_view"] : '', enableRowGroup: true, filter: 'agSetColumnFilter', width: 200, pinned: 'left' },
       { headerName: "Venta", field: "detalle_factura.valor_neto_sum", valueGetter: p => p.data ? Number(p.data["detalle_factura.valor_neto_sum"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value), sort: 'desc', filter: 'agNumberColumnFilter' },
       { headerName: "Ventas Proy.", field: "detalle_factura.ventas_proyeccion", valueGetter: p => p.data ? Number(p.data["detalle_factura.ventas_proyeccion"]) : 0, aggFunc: 'sum', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value), filter: 'agNumberColumnFilter' },
       { headerName: "Precio Unit.", field: "detalle_factura.precio_unitario", valueGetter: p => p.data ? Number(p.data["detalle_factura.precio_unitario"]) : 0, aggFunc: 'avg', enableValue: true, valueFormatter: p => currencyFormatter.format(p.value), filter: 'agNumberColumnFilter' },
